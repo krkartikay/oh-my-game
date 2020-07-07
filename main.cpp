e45fc5acf8a148fdcb1fd5a3eec6f231f64e651e
Aarsh Sharma <aarsh01rsh@gmail.com>
2020-07-07 17:18:30 +0530

    Made code a bit cleaner

--- main.cpp
+++ main.cpp
@@ -1,186 +1,220 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #include <SFML/Graphics.hpp>
 #include <time.h>
 using namespace sf;
 
 int ts = 54; //tile size
-Vector2i offset(48,24);
+Vector2i offset(48, 24);
 
 struct piece
-{ int x,y,col,row,kind,match,alpha;
-  piece(){match=0; alpha=255;}
+{
+  int x, y, col, row, kind, match, alpha;
+  piece()
+  {
+    match = 0;
+    alpha = 255;
+  }
 } grid[10][10];
 
-void swap(piece p1,piece p2)
+void swap(piece p1, piece p2)
 {
-  std::swap(p1.col,p2.col);
-  std::swap(p1.row,p2.row);
+  std::swap(p1.col, p2.col);
+  std::swap(p1.row, p2.row);
 
-  grid[p1.row][p1.col]=p1;
-  grid[p2.row][p2.col]=p2;
+  grid[p1.row][p1.col] = p1;
+  grid[p2.row][p2.col] = p2;
 }
-
 
 int main()
 {
-    srand(time(0));
-    RenderWindow app(VideoMode(700,480), "Match-3 Game!");
-    
-    app.setFramerateLimit(60);
+  srand(time(0));
+  RenderWindow app(VideoMode(700, 480), "Match-3 Game!");
 
-    Texture t1,t2;
-    t1.loadFromFile("images/background.png");
-    t2.loadFromFile("images/gems.png");
+  app.setFramerateLimit(60);
 
+  Texture t1, t2;
+  t1.loadFromFile("images/background.png");
+  t2.loadFromFile("images/gems.png");
 
-    Sprite background(t1), gems(t2);
+  Sprite background(t1), gems(t2);
 
-    Text scored, timer;
-    Font font;
-    font.loadFromFile("game_over.ttf");
-    scored.setFont(font);
-    timer.setFont(font);
-    scored.setCharacterSize(70);
-    scored.setPosition(Vector2f(500.0f, 270.0f));
-    timer.setCharacterSize(70);
-    timer.setPosition(Vector2f(600.0f, 350.0f));
-    timer.setFillColor(Color::Black);
-    scored.setFillColor(Color::Black);
+  Text scored, timer;
+  Font font;
+  font.loadFromFile("game_over.ttf");
+  scored.setFont(font);
+  timer.setFont(font);
+  scored.setCharacterSize(70);
+  scored.setPosition(Vector2f(500.0f, 270.0f));
+  timer.setCharacterSize(70);
+  timer.setPosition(Vector2f(600.0f, 350.0f));
+  timer.setFillColor(Color::Black);
+  scored.setFillColor(Color::Black);
 
-    for (int i=1;i<=8;i++)
-     for (int j=1;j<=8;j++)
+  for (int i = 1; i <= 8; i++)
+    for (int j = 1; j <= 8; j++)
+    {
+      grid[i][j].kind = rand() % 3;
+      grid[i][j].col = j;
+      grid[i][j].row = i;
+      grid[i][j].x = j * ts;
+      grid[i][j].y = i * ts;
+    }
+
+  int highest = 0;
+  int x0, y0, x, y;
+  int click = 0;
+  Vector2i pos;
+  bool isSwap = false, isMoving = false;
+  Clock c;
+  int total_time = 60;
+  while (app.isOpen())
+  {
+    Event e;
+    while (app.pollEvent(e))
+    {
+      if (e.type == Event::Closed)
+        app.close();
+
+      if (e.type == Event::MouseButtonPressed)
+        if (e.key.code == Mouse::Left)
+        {
+          if (!isSwap && !isMoving)
+            click++;
+          pos = Mouse::getPosition(app) - offset;
+        }
+    }
+
+    int rem_time = total_time - c.getElapsedTime().asSeconds();
+    if (rem_time == 0)
+    {
+      cout << highest << endl;
+      break;
+    }
+    // mouse click
+    if (click == 1)
+    {
+      x0 = pos.x / ts + 1;
+      y0 = pos.y / ts + 1;
+    }
+    if (click == 2)
+    {
+      x = pos.x / ts + 1;
+      y = pos.y / ts + 1;
+      if (abs(x - x0) + abs(y - y0) == 1)
       {
-          grid[i][j].kind=rand()%3;
-          grid[i][j].col=j;
-          grid[i][j].row=i;
-          grid[i][j].x = j*ts;
-          grid[i][j].y = i*ts;
+        swap(grid[y0][x0], grid[y][x]);
+        isSwap = 1;
+        click = 0;
+      }
+      else
+        click = 1;
+    }
+
+    //Match finding
+    for (int i = 1; i <= 8; i++)
+    {
+      for (int j = 1; j <= 8; j++)
+      {
+        if (grid[i][j].kind == grid[i + 1][j].kind)
+          if (grid[i][j].kind == grid[i - 1][j].kind)
+            for (int n = -1; n <= 1; n++)
+              grid[i + n][j].match = 1;
+
+        if (grid[i][j].kind == grid[i][j + 1].kind)
+          if (grid[i][j].kind == grid[i][j - 1].kind)
+            for (int n = -1; n <= 1; n++)
+              grid[i][j + n].match = 1;
+      }
+    }
+
+    //Moving animation
+    isMoving = false;
+    for (int i = 1; i <= 8; i++)
+      for (int j = 1; j <= 8; j++)
+      {
+        piece &p = grid[i][j];
+        int dx, dy;
+        for (int n = 0; n < 4; n++) // 4 - speed
+        {
+          dx = p.x - p.col * ts;
+          dy = p.y - p.row * ts;
+          if (dx)
+            p.x -= dx / abs(dx);
+          if (dy)
+            p.y -= dy / abs(dy);
+        }
+        if (dx || dy)
+          isMoving = 1;
       }
 
-    int highest = 0;
-    int x0,y0,x,y; int click=0; Vector2i pos;
-    bool isSwap=false, isMoving=false;
-    Clock c;
-    int total_time = 60;
-    while (app.isOpen())
+    //Deleting amimation
+    if (!isMoving)
+      for (int i = 1; i <= 8; i++)
+        for (int j = 1; j <= 8; j++)
+          if (grid[i][j].match)
+            if (grid[i][j].alpha > 10)
+            {
+              grid[i][j].alpha -= 10;
+              isMoving = true;
+            }
+
+    //Get score
+    int score = 0;
+    for (int i = 1; i <= 8; i++)
+      for (int j = 1; j <= 8; j++)
+        score += grid[i][j].match;
+
+    //Second swap if no match
+    if (isSwap && !isMoving)
     {
-        Event e;
-        while (app.pollEvent(e))
-        {
-            if (e.type == Event::Closed)
-                app.close();
-                   
-            if (e.type == Event::MouseButtonPressed)
-                if (e.key.code == Mouse::Left)
-                {
-                   if (!isSwap && !isMoving) click++;
-                   pos = Mouse::getPosition(app)-offset;
-                }
-         }
-    
-    int rem_time = total_time - c.getElapsedTime().asSeconds();
-    if(rem_time == 0){
-        cout << highest << endl;
-        break;
-    }
-   // mouse click
-   if (click==1)
-    {
-      x0=pos.x/ts+1;
-      y0=pos.y/ts+1;
-    }
-   if (click==2)
-    {
-      x=pos.x/ts+1;
-      y=pos.y/ts+1;
-      if (abs(x-x0)+abs(y-y0)==1)
-        {swap(grid[y0][x0],grid[y][x]); isSwap=1; click=0;}
-      else click=1;
+      {
+        if (!score)
+          swap(grid[y0][x0], grid[y][x]);
+        isSwap = 0;
+      }
+      for (int i = 1; i <= 8; i++)
+      {
+        for (int j = 1; j <= 8; j++)
+          grid[i][j].match = 0;
+      }
     }
 
-   //Match finding
-   for(int i=1;i<=8;i++){
-    for(int j=1;j<=8;j++)
+    //Update grid
+    if (!isMoving)
     {
-        if (grid[i][j].kind==grid[i+1][j].kind)
-        if (grid[i][j].kind==grid[i-1][j].kind)
-        for(int n=-1;n<=1;n++) grid[i+n][j].match = 1;
+      for (int i = 8; i > 0; i--)
+        for (int j = 1; j <= 8; j++)
+          if (grid[i][j].match)
+            for (int n = i; n > 0; n--)
+              if (!grid[n][j].match)
+              {
+                swap(grid[n][j], grid[i][j]);
+                break;
+              };
 
-        if (grid[i][j].kind==grid[i][j+1].kind)
-        if (grid[i][j].kind==grid[i][j-1].kind)
-        for(int n=-1;n<=1;n++) grid[i][j+n].match = 1;
-    }
-   }
-
-   //Moving animation
-   isMoving=false;
-   for (int i=1;i<=8;i++)
-    for (int j=1;j<=8;j++)
-     {
-       piece &p = grid[i][j];
-       int dx,dy;
-       for(int n=0;n<4;n++)   // 4 - speed
-       {dx = p.x-p.col*ts;
-        dy = p.y-p.row*ts;
-        if (dx) p.x-=dx/abs(dx);
-        if (dy) p.y-=dy/abs(dy);}
-       if (dx||dy) isMoving=1;
-     }
-
-   //Deleting amimation
-   if (!isMoving)
-    for (int i=1;i<=8;i++)
-    for (int j=1;j<=8;j++)
-    if (grid[i][j].match) if (grid[i][j].alpha>10) {grid[i][j].alpha-=10; isMoving=true;}
-
-   //Get score
-   int score=0;
-   for (int i=1;i<=8;i++)
-    for (int j=1;j<=8;j++)
-      score +=grid[i][j].match;
-
-   //Second swap if no match
-   if (isSwap && !isMoving){
-      {if (!score) swap(grid[y0][x0],grid[y][x]); isSwap=0;}
-       for (int i=1;i<=8;i++){
-        for (int j=1;j<=8;j++)
+      for (int j = 1; j <= 8; j++)
+        for (int i = 8, n = 0; i > 0; i--)
+          if (grid[i][j].match)
+          {
+            grid[i][j].kind = rand() % 7;
+            grid[i][j].y = -ts * n++;
             grid[i][j].match = 0;
-       }
-   }
-
-   //Update grid
-   if (!isMoving)
-    {
-      for(int i=8;i>0;i--)
-       for(int j=1;j<=8;j++)
-         if (grid[i][j].match)
-         for(int n=i;n>0;n--)
-            if (!grid[n][j].match) {swap(grid[n][j],grid[i][j]); break;};
-
-      for(int j=1;j<=8;j++)
-       for(int i=8,n=0;i>0;i--)
-         if (grid[i][j].match)
-           {
-            grid[i][j].kind = rand()%7;
-            grid[i][j].y = -ts*n++;
-            grid[i][j].match=0;
             grid[i][j].alpha = 255;
-           }
-     }
-
+          }
+    }
 
     // Draw
     app.draw(background);
 
-    for (int i=1;i<=8;i++)
-     for (int j=1;j<=8;j++)
+    for (int i = 1; i <= 8; i++)
+      for (int j = 1; j <= 8; j++)
       {
         piece p = grid[i][j];
-        gems.setTextureRect( IntRect(p.kind*49,0,49,49));
-        gems.setColor(Color(255,255,255,p.alpha));
-        gems.setPosition(p.x,p.y);
-        gems.move(offset.x-ts,offset.y-ts);
+        gems.setTextureRect(IntRect(p.kind * 49, 0, 49, 49));
+        gems.setColor(Color(255, 255, 255, p.alpha));
+        gems.setPosition(p.x, p.y);
+        gems.move(offset.x - ts, offset.y - ts);
         app.draw(gems);
       }
     stringstream ss, st;
@@ -192,6 +226,6 @@ int main()
     app.draw(scored);
     app.draw(timer);
     app.display();
-    }
-    return 0;
+  }
+  return 0;
 }
